49ddefa571e509d5eaf4f69e7bc3ce17f68dd5ca
PureDark <bruce.lee.liang@gmail.com>
2023-08-14 22:21:10 +0800

    Another missing file.

---
+++ GunfireRebornVRMod/GRVRMod/Player/Behaviours/CameraSmoother.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using VRMod.Player.MotionControlls;
+using VRMod.Settings;
+using static Cinemachine.CinemachineTargetGroup;
+
+namespace VRMod.Player.Behaviours
+{
+
+    internal class CameraSmoother : MonoBehaviour
+    {
+        public CameraSmoother(IntPtr value) : base(value) { }
+        public Transform target;
+
+        public float smoothTime = 0.05f;
+        private Quaternion deriv = Quaternion.identity;
+
+        void Awake()
+        {
+            smoothTime = ModConfig.FPCamSmoothTime.Value;
+        }
+
+        void LateUpdate()
+        {
+            if(target != null)
+            {
+                transform.position = target.position;
+                transform.rotation = transform.rotation.SmoothDamp(target.rotation, ref deriv, smoothTime);
+            }
+        }
+    }
+}
